282cc2fbd9c796d144a397e134d099390c04ae5d
chrystianMelo <chrysmelao@gmail.com>
2020-05-13 20:36:33 -0300

    Responsive translation (animation)

--- app/src/main/java/com/example/beex/view/account/AccountManagement.java
+++ app/src/main/java/com/example/beex/view/account/AccountManagement.java
@@ -3,12 +3,16 @@ package com.example.beex.view.account;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Space;
+
 import com.example.beex.R;
 import com.example.beex.view.schedule.ScheduleActivity;
 import com.example.beex.view.feed.HomeScreen;
@@ -24,6 +28,7 @@ public class AccountManagement extends AppCompatActivity {
     LinearLayout screen;
     TextInputLayout mailLayout, passLayout;
     TextInputEditText mail, pass;
+    Space topOfScreen;
     Button login;
 
     @Override
@@ -31,6 +36,7 @@ public class AccountManagement extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
+        topOfScreen = findViewById(R.id.spc_account_top);
         icon = findViewById(R.id.img_account_icon);
         screen = findViewById(R.id.ll_account_content);
         mailLayout =  findViewById(R.id.til_account_mailLayout);
@@ -39,8 +45,28 @@ public class AccountManagement extends AppCompatActivity {
         pass    =  findViewById(R.id.tiet_account_pass);
         login   = findViewById(R.id.btn_account_login);
 
+        getPosition();
         setButtonFunctions();
+    }
 
+    void getPosition(){
+        topOfScreen.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                int[] pos = new int[2];
+                topOfScreen.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                topOfScreen.getLocationOnScreen(pos);
+                if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+                    setAnimation(-1*pos[0]/2);
+                else
+                    setAnimation(-1*pos[0]);
+            }
+        });
+    }
+
+    public void setAnimation(int y) {
+        icon.animate().translationYBy(Math.round(y*0.9)).setDuration(1500);
+        screen.animate().setDuration(2500).alpha(1);
     }
 
     public int verification(String field){
